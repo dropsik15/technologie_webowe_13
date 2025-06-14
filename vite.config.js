322312bf671242c7dd4c1e0163535414ed737034
dropsik15 <bborkowskajulia@gmail.com>
2025-06-14 15:46:53 +0200

    vite i package

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,45 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import fg from 'fast-glob'
+//import { glob } from 'node:fs/promises'
+import { readdir, stat } from 'node:fs/promises'
 import tailwindcss from '@tailwindcss/vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const inputs = fg.sync('src/**/*.html')
+async function findHtmlFiles(dir) {
+  let results = []
+  const entries = await readdir(dir, { withFileTypes: true })
+
+  for (const entry of entries) {
+    const fullPath = resolve(dir, entry.name)
+    if (entry.isDirectory()) {
+      const subFiles = await findHtmlFiles(fullPath)
+      results = results.concat(subFiles)
+    } else if (entry.isFile() && fullPath.endsWith('.html')) {
+      results.push(fullPath)
+    }
+  }
+
+  return results
+}
+
+const inputs = await findHtmlFiles(resolve(__dirname, 'src'))
+
+console.log(inputs)
 
 export default defineConfig({
-  base: '/technologie_webowe_13/',
   plugins: [
     tailwindcss(),
   ],
+
+  base: '/technologie_webowe_13/',
+
   root: resolve(__dirname, 'src'),
   build: {
     emptyOutDir: true,
     rollupOptions: {
-      input: inputs.map((file) => resolve(__dirname, file)),
+      input: inputs,
     },
     outDir: resolve(__dirname, 'dist'),
   },
